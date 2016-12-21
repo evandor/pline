6aa722ea303327a69d4dd7a7e596549cdae43084
Carsten Graef <evandor@gmail.com>
2016-12-21 08:43:34 +0100

    branching test approach, not working yet

---
+++ src/providers/tracking-service.spec.ts
@@ -0,0 +1,12 @@
+import { inject} from '@angular/core/testing';
+import {MagicBall} from './magic-ball';
+ 
+describe('Magic 8 Ball Service', () => {
+ 
+    it('should do nothing', () => {
+ 
+        expect(true).toBeTruthy();
+ 
+    });
+ 
+});
