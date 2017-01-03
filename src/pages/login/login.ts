594bc61d0fb734d0eb28f8cc97e662d00c5b2c08
IzzyGray <mira@skysail.io>
2017-01-03 14:37:17 +0100

    login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,24 +1,82 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, LoadingController, AlertController } from 'ionic-angular';
+import { FormBuilder, Validators } from '@angular/forms';
+import { SignUpPage } from '../sign-up/sign-up';
+import { HomePage } from '../home/home';
+import { EmailValidator } from '../../validators/email';
+import firebase from 'firebase';
 
-/*
-  Generated class for the Login page.
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  public loginForm;
+  emailChanged: boolean = false;
+  passwordChanged: boolean = false;
+  submitAttempt: boolean = false;
+  loading: any;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+  constructor(public navCtrl: NavController,
+    public formBuilder: FormBuilder,
+    public alertCtrl: AlertController,
+    public loadingCtrl: LoadingController) {
+
+    this.loginForm = formBuilder.group({
+      email: ['', Validators.compose([Validators.required, EmailValidator.isValid])],
+      password: ['', Validators.compose([Validators.minLength(6), Validators.required])]
+    });
   }
 
- 
+  elementChanged(input) {
+    let field = input.inputControl.name;
+    this[field + "Changed"] = true;
+  }
+
+  loginUser() {
+
+    this.submitAttempt = true;
+
+    if (!this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    } else {
+      firebase.auth().signInWithEmailAndPassword(this.loginForm.value.email, this.loginForm.value.password).then(authData => {
+        this.navCtrl.setRoot(HomePage);
+      }, error => {
+        this.loading.dismiss().then(() => {
+          let alert = this.alertCtrl.create({
+            message: error.message,
+            buttons: [
+              {
+                text: "Ok",
+                role: 'cancel'
+              }
+            ]
+          });
+          alert.present();
+        });
+      });
+
+      this.loading = this.loadingCtrl.create({
+        dismissOnPageChange: true,
+      });
+      this.loading.present();
+    }
+  }
+
+  goToSignup() {
+    this.navCtrl.push(SignUpPage);
+  }
+
+  /*goToResetPassword() {
+    this.navCtrl.push(ResetPasswordPage);
+  }*/
+
+
+
+
+
 
 }
