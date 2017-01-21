8dbdfbe202e5f51d566319449715ff33248b77d4
Carsten Graef <evandor@gmail.com>
2017-01-21 09:48:53 +0100

    wip

--- rebuild.sh
+++ rebuild.sh
@@ -4,5 +4,10 @@ mkdir www
 
 npm install
 ionic platform add ios
+
+ionic plugin add cordova-plugin-x-socialsharing
+
+cordova plugin add ionic-plugin-deeplinks --variable URL_SCHEME=pline --variable DEEPLINK_SCHEME=https --variable DEEPLINK_HOST=pline.one
+
 ionic build
 
